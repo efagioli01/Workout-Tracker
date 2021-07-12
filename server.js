ecb7d52ce358b4d883356fd39c5412c8ad600764
Erica Fagioli <efagioli01@gmail.com>
2021-07-12 12:33:52 -0400

    deploying to heroku

--- server.js
+++ server.js
@@ -11,10 +11,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-mongoose.connect('mongodb://localhost/workout', {
-    useNewUrlParser: true,
-    useFindAndModify: false
-})
+// mongoose.connect('mongodb://localhost/workout', {
+//     useNewUrlParser: true,
+//     useFindAndModify: false
+// })
+
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout-tracker',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false 
+    }
+);
 
 app.use(require('./routes/apiRoutes'));
 app.use(require('./routes/htmlRoutes'));
